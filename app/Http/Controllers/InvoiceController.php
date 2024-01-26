a7a716fe1223795a1730d94fd5ed710d703bf728
dquangxyz <dq2209@gmail.com>
2024-01-26 11:02:40 +1100

    render list of invoices

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Invoice;
 
 class InvoiceController extends Controller
 {
-    //
+    public function get_all_invoice() {
+        // $invoices = Invoice::all(); // this means SELECT * FROM invoices
+
+        $invoices = Invoice::with('customer')->orderBy('id','DESC')->get(); 
+        // this means
+        // SELECT invoices.*, customers.*
+        // FROM invoices
+        // LEFT JOIN customers ON invoices.customer_id = customers.id
+        // ORDER BY invoices.id DESC;
+        
+        return response()->json([
+            'invoices' => $invoices
+        ], 200);
+    }
 }
